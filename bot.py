232fab477e37e1529a41e65df97047d94ade9003
delaynieym <delaynieym@outlook.com>
2020-05-19 23:52:34 -0700

    cleaned up keval's "logic"
    key reads from untracked file now

--- bot.py
+++ bot.py
@@ -1,19 +1,21 @@
+from keyhouse import *
 import discord
 import random
+whitelistChannels = [530491424529973260, 675993960178647040, 569586147160883241]
 emojis = ["😂","👀", "👏", "✔", "😢", "💕", "👍", "🙌", "🤞", "🎁", "🤔", "🤣", "💋", "🎶", "🎉", "😊"]
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as', self.user)
     async def on_message(self, message):
         print("message received")
-        if (message.channel.id != 530491424529973260) and (message.channel.id != 675993960178647040) and (message.channel.id != 569586147160883241):
-            print("not allowed")
-            print(message.channel.id)
-            return
+        if (message.channel.id not in whitelistChannels):
+           print("not allowed")
+           print(message.channel.id)
+           return
         print("allowed")
         r = random.randint(1,20)
         if r == 1:
             for em in emojis:
                 await message.add_reaction(em)
 client = MyClient()
-client.run('You\'re token here')
+client.run(ghostKey)
